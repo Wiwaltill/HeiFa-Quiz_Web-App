0f2dc61bab590dba926f4669f8a95c0b3d8c3194
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2022-09-05 23:27:16 +0200

    Change script links
    - Remote script from CDN
    - Update Version

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
         <a id="dateschutz" href="datenschutz.php" target="_blank"><p>Datenschutz</p></a>
       </div>
 </footer>
-<script src="assets/scripts/jquery-3.5.1.js"></script>
-<script src="assets/scripts/bootstrap.bundle.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
