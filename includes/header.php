0e2e677f157f7e253c698c06b22bd2ca7a17878f
TiLL Esser <74987472+Wiwaltill@users.noreply.github.com>
2022-09-05 23:28:12 +0200

    Update fontawesome link
    - Update to script
    - Use script from CDN

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link preload href="assets/style/main.css" type="text/css" rel="stylesheet" />
 		<link preload href="assets/style/style.css" type="text/css" rel="stylesheet" />
-		<link preload href="assets/style/fontawesome.css" type="text/css" rel="stylesheet" />
+		<script src="https://kit.fontawesome.com/cf33211e1e.js" crossorigin="anonymous"></script>
+		<!--<link preload href="assets/style/fontawesome.css" type="text/css" rel="stylesheet" />
 		<link preload href="assets/style/bootstrap.css" type="text/css" rel="stylesheet" />
 		<!--<script src="/heifa/assets/scripts/phonetest.js"></script>-->
 		
